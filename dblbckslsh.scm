c6ee97f4a6fb515ac9d1d9bf20f026e83c55f830
Zach Flynn <zlflynn@gmail.com>
2018-10-26 22:51:36 -0400

    Initial commit

---
+++ dblbckslsh.scm
@@ -0,0 +1,76 @@
+(use-modules (ice-9 regex))
+(use-modules (ice-9 textual-ports))
+(define header-regex "\\\\\\\\HEADER")
+(define footer-regex "\\\\\\\\FOOTER")
+
+(define page-list
+  (list "index" "research" "teaching" "programs" "links" "102" "310"))
+
+(map
+ (lambda (page)
+
+   (define file
+     (string-append page ".html"))
+
+   (define src
+     (string-append page ".pre.html"))
+
+   (define header-text
+     "<h1>Zach Flynn</h1>\n
+  <hr>\n
+  <a href=\"index.html\">Home</a>\n
+  <a href=\"research.html\">Research</a>\n
+  <a href=\"teaching.html\">Teaching</a>\n
+  <a href=\"programs.html\">Programs</a>\n
+  <a href=\"links.html\">Links</a>\n
+  <a href=\"https://medium.com/@zlflynn\">Blog</a>\n
+  <hr>\n
+  <a href=\"https://github.com/flynnzac/cv/raw/master/cv.pdf\">Curriculum Vitae</a>\n
+  <address>\n
+    Email: <a href=\"mailto:zlflynn@gmail.com\">zlflynn@gmail.com</a>\n
+  </address>\n
+  <hr>\n")
+
+   (define footer-text
+     (string-append "\n<hr>\nThis site last update: "
+                    (strftime "%c" (localtime (current-time)))))
+
+   (define regex-text
+     (string-append "<a href=\\\""
+                    file
+                    "\\\">([a-zA-Z]+)</a>"))
+
+
+   (define main-text (get-string-all (open-file src "r")))
+   (let ((cur-match (string-match regex-text header-text)))
+     (if cur-match
+         (set! header-text
+           (regexp-substitute #f cur-match 'pre
+                              (string-append "<b><a href=\"" file "\">") 1 "</a></b>\n" 'post))))
+
+
+
+
+
+   (set! main-text
+     (regexp-substitute #f (string-match header-regex main-text)
+                        'pre
+                        header-text
+                        'post))
+
+   (set! main-text
+     (regexp-substitute #f (string-match footer-regex main-text)
+                        'pre
+                        footer-text
+                        'post))
+
+
+   
+   (let ((output-port (open-file file "w")))
+     (display main-text output-port)
+     (close-port output-port)))
+ page-list)
+
+
+
+
