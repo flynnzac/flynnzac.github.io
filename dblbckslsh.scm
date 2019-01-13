d98b433afee27dc0ba71d4e3e5dffd5a6dc2c42e
Zach Flynn <zlflynn@gmail.com>
2019-01-12 21:00:46 -0500

    Added reading

--- dblbckslsh.scm
+++ dblbckslsh.scm
@@ -6,7 +6,7 @@
 (use-modules (ice-9 textual-ports))
 
 (define page-list
-  (list "index" "research" "teaching" "programs" "links" "102" "310"))
+  (list "index" "research" "teaching" "programs" "links" "102" "310" "reading"))
 
 (define-syntax create-pattern
   (syntax-rules ()
@@ -65,6 +65,7 @@
   <a href=\"teaching.html\">Teaching</a>
   <a href=\"programs.html\">Programs</a>
   <a href=\"links.html\">Links</a>
+  <a href=\"reading.html\">Reading</a>
   <hr>
   <a href=\"https://github.com/flynnzac/cv/raw/master/cv.pdf\">Curriculum Vitae</a><br>
   <address>
@@ -83,15 +84,15 @@
 	    header-text))))
 
 
-   (define main-text (get-string-all (open-file src "r")))
-
-   (set! main-text (header-replace main-text))
-   (set! main-text (footer-replace main-text))
-   (set! main-text (analytics-replace main-text))
-   (set! main-text (meta-replace main-text))
-
-   (let ((output-port (open-file file "w")))
-     (display main-text output-port)
+   (let ((output-port (open-file file "w"))
+	 (main-text (get-string-all (open-file src "r"))))
+     (display
+      (meta-replace
+       (analytics-replace
+	(footer-replace
+	 (header-replace
+	  main-text))))
+      output-port)
      (close-port output-port)))
  page-list)
 
